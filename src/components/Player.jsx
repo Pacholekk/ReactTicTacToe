f809f9a6d9ec0ee3480a1741b37f9b0dbf19d936
Pacholekk <patryk.pacholski2001@gmail.com>
2025-02-11 12:15:40 +0100

    Complete name change and save

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,30 @@
-export default function Player({ name, symbol }) {
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setisEditing] = useState(false);
+  function handleClick() {
+    setisEditing((prevState) => !prevState);
+  }
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+  let editPlayerName = <span className="player-name">{playerName}</span>;
+  let btnCaption = "Edit";
+
+  if (isEditing) {
+    editPlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+    btnCaption = "Save";
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {editPlayerName}
         <span className="player-symbol">{symbol}</span>
-        <button>Edit</button>
+        <button onClick={handleClick}>{btnCaption}</button>
       </span>
     </li>
   );
